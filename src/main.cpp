54d6a004a864e148500a54bbc76a9c9267506f81
Yuma Matsune <yuma.matsune@gmail.com>
2018-09-28 19:30:09 +0900

    parse cmd flag, be able to assign .conf path

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "conf_parser.hpp"
 #include "pman.hpp"
+#include "../lib/cmdline/cmdline.h"
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-  string confFile = "pman.conf";
+  cmdline::parser cmd;
+  cmd.add<string>("conf", 'c', "pman.conf file path", false, "./pman.conf");
+  cmd.parse_check(argc, argv);
+
+  string confFile = cmd.get<string>("conf");
   ConfParser parser(confFile);
   if (parser.ParseError()) {
     cerr << "Can't load conf file." << endl;
