722054652e419dc9dd1e8bf0fa18c0a04b7383e5
Yuma Matsune <yuma.matsune@gmail.com>
2018-10-03 11:14:35 +0900

    fixed parser

--- src/cmd_parser.cpp
+++ src/cmd_parser.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-#include "util.hpp"
 #include "defines.h"
 #include "cmd_parser.hpp"
 
@@ -9,7 +8,7 @@ using namespace std;
 
 void CmdParser::parse()
 {
-  if (argc < 2) return;
+  if (!consume()) return;
 
   if (isVersion()) {
     showVersion();
@@ -21,12 +20,22 @@ void CmdParser::parse()
     exit(0);
   }
 
-  parseOption();
-  if (!parseCommand()) {
-    return;
+  if (isCommand()) {
+    parseCommand();
+    if (!consume()) return;
   }
+
   parseArgs();
 }
+
+bool CmdParser::consume()
+{
+  if (hasNext()) {
+   cursor++;
+   return true;
+  }
+  return false;
+};
 
 bool CmdParser::hasNext()
 {
@@ -35,25 +44,7 @@ bool CmdParser::hasNext()
 
 bool CmdParser::match(const char *str)
 {
-  return strcmp(argv[cursor], str) == 0;
-}
-
-bool CmdParser::isOption()
-{
-  return isVersion() || isHelp();
-}
-
-void CmdParser::parseOption()
-{
-  if (!isOption()) return;
-
-  if (isVersion()) {
-    showVersion();
-  } else if (isHelp()) {
-    cout << usage() << endl;
-  }
-
-  exit(0);
+  return strcmp(current(), str) == 0;
 }
 
 bool CmdParser::isCommand()
@@ -61,10 +52,9 @@ bool CmdParser::isCommand()
   return match("status") || match("start") || match("stop");
 }
 
-// returns true if continue parsing
-bool CmdParser::parseCommand()
+void CmdParser::parseCommand()
 {
-  if (!isCommand()) return true;
+  assert(isCommand());
 
   if (match("status")) {
     this->command_ = E_STATUS;
@@ -74,76 +64,65 @@ bool CmdParser::parseCommand()
     this->command_ = E_STOP;
   }
 
-  switch (this->command_) {
-    case E_START:
-    case E_STOP:
-      if (!hasNext()) {
-        // no program name
+  // parse program name
+
+  // `start` and `stop` command requires program name.
+  // `status` command allows empty, then called `status all` by default.
+  if (!hasNext()) {
+    switch (command()) {
+      case E_START:
+      case E_STOP:
         cerr << "requires program name" << endl << endl;
         cerr << usage() << endl;
         exit(1);
-      }
-      cursor++;
-
-      if (startsWith(argv[cursor], "-")) {
-        cerr << "invalid program name: " << argv[cursor] << endl;
-        exit(1);
-      }
-
-      if (match("all")) {
-        switch (this->command_) {
-          case E_START:
-            this->command_ = E_START_ALL;
-            break;
-          case E_STOP:
-            this->command_ = E_STOP_ALL;
-            break;
-          default:
-            break;
-        }
-      } else {
-        this->program_ = string(argv[cursor]);
-      }
-      break;
-    default:
-      break;
+      default:
+        return;
+    }
   }
 
+  consume();
 
-  if (hasNext()) {
-    cursor++;
-    return true;
-  } else {
-    return false;
+  if (isArgs()) {
+    if (command() == E_START || command() == E_STOP) {
+      cerr << "requires program name" << endl;
+      exit(1);
+    } else {
+      back();
+      return;
+    }
   }
+
+  this->program_ = string(current());
+}
+
+bool CmdParser::isArgs()
+{
+  return isConf();
+}
+
+bool CmdParser::isConf()
+{
+  return match("--conf") || match("-c");
 }
 
 void CmdParser::parseArgs()
 {
   for (;;) {
-    if (match("--conf") || match("-c")) {
-      parseConf();
+    if (isConf()) {
+      if (!consume()) {
+        cerr << "please pass conf file path after `--conf` or `-c`" << endl;
+        exit(1);
+      }
+
+      this->conffile_ = string(current());
     } else {
-      cerr << "unknown option: " << argv[cursor] << endl << endl;
+      cerr << "unknown option: " << current() << endl << endl;
       cerr << usage() << endl;
       exit(1);
     }
 
-    if (!hasNext()) break;
-
-    cursor++;
+    if (!consume()) return;
   }
-}
-
-void CmdParser::parseConf()
-{
-  if (!hasNext()) {
-    cerr << "please pass conf file path after `--conf` or `-c`" << endl;
-    exit(1);
-  }
-
-  cursor++;
-  this->conffile_ = string(argv[cursor]);
 }
 
 bool CmdParser::isVersion()
@@ -167,7 +146,8 @@ std::string CmdParser::usage()
   os << "usage: pman [--version | -v] [--help | -h]" << endl
     << "\t    <command> [<args>]" << endl
     << "command:" << endl
-    << "  status          \tshow status" << endl
+    << "  status <program>\tshow status" << endl
+    << "  status all      \tshow status of all programs" << endl
     << "  start <program> \tstart program" << endl
     << "  start all       \tstart all programs that not running" << endl
     << "  stop <program>  \tstop program" << endl
--- src/cmd_parser.hpp
+++ src/cmd_parser.hpp
@@ -4,9 +4,7 @@ enum Command {
   E_DAEMON,
   E_STATUS,
   E_START,
-  E_START_ALL,
-  E_STOP,
-  E_STOP_ALL
+  E_STOP
 };
 
 class CmdParser {
@@ -17,14 +15,17 @@ private:
   Command command_;
   std::string program_;
   std::string conffile_;
+  // step cursor if cursor is not the end
+  bool consume();
+  void back() { cursor--; }
+  char *current() { return argv[cursor]; }
   bool hasNext();
   bool match(const char *str);
-  bool isOption();
-  void parseOption();
   bool isCommand();
-  bool parseCommand();
+  void parseCommand();
+  bool isArgs();
   void parseArgs();
-  void parseConf();
+  bool isConf();
   bool isVersion();
   bool isHelp();
   void showVersion();
@@ -32,7 +33,7 @@ private:
 
 public:
   CmdParser(int argc, char **argv)
-    : argc(argc), argv(argv), cursor(1), command_(E_DAEMON), program_(""), conffile_("./pman.conf") {};
+    : argc(argc), argv(argv), cursor(0), command_(E_DAEMON), program_(""), conffile_("./pman.conf") {};
   void parse();
   Command command() { return this->command_; }
   std::string conffile() { return this->conffile_; }
